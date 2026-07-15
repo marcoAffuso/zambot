174c57c6b480fd1af1ce0be02fd75c85145a74e6
Marco Affuso <marco.affuso@emeal.nttdata.com>
2026-07-15 09:47:24 +0200

    aggiunto time out per testare le api di jenkins

--- src/test/java/com/surgi/steps/LoginSteps.java
+++ src/test/java/com/surgi/steps/LoginSteps.java
@@ -9,6 +9,8 @@ import com.surgi.utility.TestContext;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginSteps {
     private LoginPage loginPage;
 
@@ -17,7 +19,10 @@ public class LoginSteps {
     }
 
     @Given("I open the login page {string}")
-    public void openLoginPage(String url) {
+    public void openLoginPage(String url) throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(60); // Wait for 60 seconds before opening the login page
+
         loginPage.open(url);
         loginPage.get();
     }
