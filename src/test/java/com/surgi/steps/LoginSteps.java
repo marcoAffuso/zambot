52e9bafcd050bba5723b0368b998582f3a129688
Marco Affuso <marco.affuso@emeal.nttdata.com>
2026-09-03 10:56:50 +0200

    ripristinato test originario

--- src/test/java/com/surgi/steps/LoginSteps.java
+++ src/test/java/com/surgi/steps/LoginSteps.java
@@ -4,6 +4,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.surgi.pages.LoginPage;
 import com.surgi.utility.TestContext;
 
@@ -17,31 +20,30 @@ public class LoginSteps {
     @Given("I open the login page {string}")
     public void openLoginPage(String url) {
         loginPage.open(url);
-        //loginPage.get();
     }
 
     @When("I enter username {string}")
     public void enterUsername(String username) {
-        //loginPage.enterUsername(username);
+        loginPage.enterUsername(username);
 
         System.out.println(username);
     }
 
     @And("I enter password {string}")
     public void enterPassword(String password) {
-        //loginPage.enterPassword(password);
+        loginPage.enterPassword(password);
         System.out.println(password);
     }
 
     @And("I click the login button")
     public void clickLoginButton() {
-        //loginPage.submit();
+        loginPage.submit();
         System.out.println("Login button clicked");
     }
 
     @Then("I verify an error message containing {string}")
     public void verifyErrorMessage(String expected) {
-        //assertTrue(loginPage.getErrorMessage().contains(expected));
+        assertTrue(loginPage.getErrorMessage().contains(expected));
         System.out.println("Error message verified: " + expected);
     }
 }
